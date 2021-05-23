156989c1a11354a9d9a3f7a4c1da7ed19c08a75c
jmoggridge <ja.moggridge@gmail.com>
2021-05-23 03:09:23 -0400

    parsed ICEberg fasta of ICE proteins; only some are integrases

---
+++ code/get_ICEberg_protein_data.R
@@ -0,0 +1,47 @@
+library(Biostrings)
+library(tidyverse)
+ 
+## The ICEberg2.0 database has a set of 26,566 proteins associated with integrative conjugative genetic elements (ICEs) as those or predicted to be.
+
+# Save a fasta file with all the protein sequences from iceberg
+writeLines(readLines("https://db-mml.sjtu.edu.cn/ICEberg2/download/ICE_aa_all.fas"), con = file("./Data/ICEberg_all_ICEs_protein.fasta"))
+
+# Download fasta of all ICEberg protein sequences for filtering
+ice_fasta <- readDNAStringSet("https://db-mml.sjtu.edu.cn/ICEberg2/download/ICE_aa_all.fas")
+
+# Create a dataframe to filter integrases from all ICEberg proteins
+ice_df <- 
+  tibble(
+    name = names(ice_fasta),
+    seq = paste(ice_fasta),
+    type = "ICE"
+  ) %>% 
+  # extract annotation info from sequence headers
+  mutate(
+    header = map(name, ~unlist(str_split(.x, "\\|"))),
+    database = map_chr(header, 1),
+    gi = map_chr(header, 2),
+    not_sure_what_this_is = map_chr(header, 3),
+    # seqs from genbank, refseq, emb, dbj
+    source = map_chr(header, 4),
+    accession = map_chr(header, 5),
+    annotation = map_chr(header, 6),
+    # which ICE
+    element = str_extract(annotation, "\\[.+\\]"),
+    element = str_remove_all(element, "\\[|\\]"),
+    # protein annotation
+    protein = str_remove(annotation, " \\[.+\\]")
+  ) %>% 
+  select(-annotation)
+glimpse(ice_df)  
+
+integrase <- ice_df %>% 
+  filter(str_detect(protein, 'integrase'))
+  
+summary(factor(ice_df$source))
+
+view(
+  ice_df %>% 
+    group_by(protein) %>% 
+    filter()
+)
