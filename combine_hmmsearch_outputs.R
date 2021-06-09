364017484269c4aba24c0739189ba23e453b02a4
jmoggridge <ja.moggridge@gmail.com>
2021-06-09 00:46:16 -0400

    created the dataframe I wanted for hmmsearch results. - Could add more domains to the set of hmms; - Could add some non-tyr integrase seqs - Could demo on some unknown seqs from Iceberg

--- combine_hmmsearch_outputs.R
+++ combine_hmmsearch_outputs.R
@@ -1,35 +1,24 @@
-
-
 library(tidyverse)
 library(Biostrings)
 library(rhmmer)
 
 # All the full protein sequences for 20 subfamilies from SMART
+# Unnest and split protein names to match hmmsearch results
 integrases <- 
-  tibble(path = Sys.glob('./data/SMART/full_protein_fasta/*.fasta')) |> 
-  mutate(
-    domain_name = str_remove_all(path, './data/SMART/full_protein_fasta/|\\_proteins.fasta'),
-    fasta = map(path, readAAStringSet),
-    n_protein = map_int(fasta, ~length(.x)),
-    prot_name = map(fasta, ~names(.x)),
-    prot_seq = map(fasta, paste)
-  ) |> 
-  # add accession numbers
-  left_join(read_csv('./data/SMART/SMART_YR_ids.csv') |> 
-              janitor::clean_names(), by = 'domain_name',
-  ) 
-
-write_rds(integrases, './data/SMART_integrases.rds')
-
-integrases |>  
-  select(domain_name, id, prot_name) |> 
+  read_rds('./data/SMART_integrases.rds') |> 
+  select(subfamily, id, prot_name) |> 
   unnest(cols = prot_name) |> 
-  mutate(prot)
+  mutate(temp = trimws(str_extract(prot_name, '.*? ')),
+         prot_description = str_remove(prot_name,  '.*?\\s'),
+         prot_name = temp) |> 
+  select(-temp)
 
+# want to join the 20 scores for each subfamily to each ref seq
 integrases
 
 
-
+# read hmmsearch tblout result
+# keep only the best_domain_score column, renamed to the subfamily name
 read_hmmersearch_tbl <- function(path){
   read_table(
     file = path,
@@ -50,41 +39,53 @@ read_hmmersearch_tbl <- function(path){
     unnest(cols = c())
 }
 
+hmmsearches <- 
+  # read all the results and join into one table
+  Sys.glob("./data/SMART/hmmsearch_res/*.search.tbl") |> 
+  map(read_hmmersearch_tbl) |> 
+  purrr::reduce(left_join, by = 'prot_name') |> 
+  # join to ref_integrases table
+  right_join(integrases, by = 'prot_name') |> 
+  select(prot_name, prot_description, subfamily, id, everything())
 
-# one table
-subfam <- 'Arch1'
-
-eg_tab <- 
-  read_hmmersearch_tbl("./data/SMART/hmmsearch_res/Arch1.search.tbl")
-  
-eg_tab
-
-left_join()
-
-
+glimpse(hmmsearches)
 
 
+hmmsearches |> 
+  pivot_longer(cols = c(Arch1:Xer), 
+               names_to = 'hmm_dom', values_to = 'hmm_score') |> 
+  mutate(hmm_score = replace_na(hmm_score, 0)) |> 
+  mutate(highlight = ifelse(hmm_dom == subfamily, T, F)) |> 
+  ggplot(aes(x = hmm_score, y = fct_rev(hmm_dom),
+             color = highlight)) +
+  geom_violin() +
+  facet_wrap(~subfamily) +
+  labs(x = 'Best domain bit score', y = 'Hidden Markov model', 
+       subtitle = 'HMM bit score profiles for each set of tyrosine integrases from the SMART subfamilies') +
+  theme_light() + 
+  theme(legend.position = 'null', 
+        axis.text.y = element_text(size = 5))
 
 
-# all hmmsearch output
-hmmsearch_rs <- 
-  tibble(path = Sys.glob('./data/SMART/hmmsearch_res/*.tbl')) |> 
-  mutate(domain_name = str_extract(path, '[^\\s|!/]+.search.tbl'),
-         domain_name = str_remove(domain_name, '.search.tbl')) |> 
-  select(domain_name, path) |> 
-  arrange(domain_name) |> 
-  mutate(result = map(path, ~read_hmmertbl(.x)))
-  # unnest(cols = c(result))
-
-glimpse(hmmsearch_rs)
-
-
-search_tbl <-
-  rhmmer::read_tblout("./data/SMART/hmmsearch_res/Arch1.search.tbl") |> 
-  select(-contains('accession'), -contains('domain_number_')) |>
-  mutate(protein = paste(domain_name, description))
-
-names(search_tbl)
-glimpse(search_tbl)
-
+# # all hmmsearch output
+# hmmsearch_rs <- 
+#   tibble(path = Sys.glob('./data/SMART/hmmsearch_res/*.tbl')) |> 
+#   mutate(domain_name = str_extract(path, '[^\\s|!/]+.search.tbl'),
+#          domain_name = str_remove(domain_name, '.search.tbl')) |> 
+#   select(domain_name, path) |> 
+#   arrange(domain_name) |> 
+#   mutate(result = map(path, ~read_hmmertbl(.x)))
+#   # unnest(cols = c(result))
+# 
+# glimpse(hmmsearch_rs)
+# 
+# 
+# search_tbl <-
+#   rhmmer::read_tblout("./data/SMART/hmmsearch_res/Arch1.search.tbl") |> 
+#   select(-contains('accession'), -contains('domain_number_')) |>
+#   mutate(protein = paste(domain_name, description))
+# 
+# names(search_tbl)
+# glimpse(search_tbl)
+# 
 
