da913cc742da590b5215fe493c276dd7e008c433
jmoggridge <ja.moggridge@gmail.com>
2021-06-21 02:05:17 -0400

    added function for rentrez queries to 1b_non_integrase script

--- code/1b_get_refseq_non_integrases.R
+++ code/1b_get_refseq_non_integrases.R
@@ -1,11 +1,73 @@
 ### Script to retrieve non-integrases from RefSeq
+### Get non-integrase proteins for 'other' class for classifier to serve as negative examples.
 
 library(furrr)
 library(Biostrings)
 library(tidyverse)
+library(rentrez)
+library(beepr)
 
-# functions I wrote get ncbi fasta records
-source('./code/ncbi_entrez_functions.R')
+
+#### Functions for NCBI downloads ----
+
+# `get_ncbi_ids` does a basic NCBI search, retrieves metadata and record ids. Sets up the fetching process in get_ESummary_df and get_Efasta functions.
+get_ncbi_ids <- function(searchexp, db){
+  
+  # find out how many ids available from 1st search
+  Esearch <- rentrez::entrez_search(
+    db = db, 
+    term = searchexp, 
+    retmax = 100)
+  # Use the 'count' from first search to get all ids;
+  # Get the web history token with 'use_history = TRUE'
+  Esearch2 <- rentrez::entrez_search(
+    db = db, 
+    term = searchexp, 
+    retmax = Esearch$count,
+    use_history = TRUE)
+  return(Esearch2)
+}
+
+# get_Efasta gets all sequences for a search expression,
+# returns a dataframe with title and sequence columns
+get_Efasta <- function(searchexp, db, apikey, fetch_max = 1600,
+                       verbose = TRUE) {
+  basic_search <- get_ncbi_ids(searchexp, db = db)
+  webhist <- basic_search$web_history
+  id.count <- min(basic_search$count, fetch_max)
+  message('Search expression: ', searchexp)
+  message(basic_search$count, ' records are available. Fetching ', id.count, ' fasta records')
+  
+  # Get 500 fasta records per iteration, concatenate them
+  fasta <- furrr::future_map(
+    seq(1, id.count, 500),
+    ~{
+      entrez_fetch(
+        db = db, 
+        web_history = webhist,
+        retstart = .x-1, 
+        retmax=500, 
+        api_key = apikey,
+        rettype = 'fasta')
+    }) |> 
+    purrr::reduce(paste0)
+  
+  # split fasta into lines and write a temp file
+  tmp <-  tempfile()
+  write(fasta, tmp)
+  # read lines as a DNAStringSet object using Biostrings package
+  fasta <- Biostrings::readBStringSet(tmp)
+  # arrange fasta into df
+  fasta.df <- tibble(title = names(fasta), seq = paste(fasta))
+  
+  if (verbose) 
+    print(fasta)
+  message('Finished query')
+  
+  return(list(fasta = fasta, df = fasta.df))
+}
+
+
 
 # list of proteins
 queries <- list(
--- code/3_join_datasets.R
+++ code/3_join_datasets.R
@@ -1,16 +1,32 @@
+
 ## join integrase and non-integrase data
 
+library(tidyverse)
+library(rsample)
 
 # smart data
 smart_train <- read_rds('./data/SMART/smart_train.rds')
 smart_test <- read_rds('./data/SMART/smart_test.rds')
 
-
-
-try <- Biostrings::readAAStringSet('./data/SMART/smart_train.fa')
-try
-
 # non_integrases
+non_integrase <- read_rds('./data/non_integrases_df.rds') |> 
+  select(-title) |> 
+  rename(group = name,
+         prot_seq = seq,
+         acc = prot_name,
+         description = prot_description) |> 
+  mutate(subfamily = 'non_integrase')
+
+glimpse(non_integrase)
+
+non_integrase |> 
+  pull(prot_seq) |> 
+  toupper() |> 
+  str_count('X') |> 
+  summary()
+
+nonint_split <- initial_split(non_integrase)
+nonint_train <- 
 
 
 ## create fasta file for HMM scoring
