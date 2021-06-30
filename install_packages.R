aea8b628219c7b392b4a875cf25f555c1c1cde07
jmoggridge <ja.moggridge@gmail.com>
2021-06-30 03:35:55 -0400

    renaming things again

---
+++ install_packages.R
@@ -0,0 +1,11 @@
+
+install.packages('BiocManager')
+BiocManager::install(version = "3.13")
+BiocManager::install("Biostrings")
+BiocManager::install("DECIPHER")
+install.packages("rpart")
+install.packages('glmnet')
+install.packages('kknn')
+install.packages('beepr')
+
+beepr::beep()
