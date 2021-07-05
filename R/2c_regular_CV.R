da1c784ef2116a3bcddb2138982264ca93310aa9
jmoggridge <ja.moggridge@gmail.com>
2021-07-05 04:56:44 -0400

    Solved the issue with 2c_regCV freezing because of unnested fold_res dataframe. Scripted final classifier prep. Still need to choose which models to evaluate; or do all???

---
+++ R/2c_regular_CV.R
@@ -0,0 +1,124 @@
+library(tidyverse)
+library(tidymodels)
+library(Biostrings)
+library(future)
+library(furrr)
+library(DECIPHER)
+library(glue)
+library(here)
+library(crayon)
+library(tictoc)
+library(beepr)
+
+# data prep and modelling functions
+source('./R/00_functions.R')
+# set of models with grid search
+source('./R/00_get_model_specs.R')
+
+# make directories for prep files
+out_path <- '3x3_regular_CV_07-02'
+make_dirs(out_path)
+system(glue('mkdir ./results/{out_path}/'))
+
+# same object forom 2b_nested_cv.R
+nest_cv <- read_rds('./results/nest_cv_06-30/3x3-fold_06-30_nest_cv_results.rds')
+
+outer_cv <- nest_cv |> 
+  select(outer_id, outer_splits)
+outer_cv
+rm(nest_cv)
+
+
+## Do CV -----
+
+# prepare scored datasets for each fold
+#  training split > align, build HMM, score sequences, model 
+tic()
+cv_prep <- outer_cv |> 
+  mutate(prep = map2(.x = outer_splits, 
+                     .y = outer_id, 
+                     .f = ~prep_data(.x, .y, out_path = out_path))
+         )
+beepr::beep()
+toc()
+write_rds(cv_prep, glue('./results/{out_path}/cv_prep.rds'), compress = 'gz')
+
+cv_prep <- read_rds(glue('./results/{out_path}/cv_prep.rds'))
+# unnest train and test columns
+cv_prep <- cv_prep |> 
+  unnest(prep) |> 
+  select(-outer_splits)
+
+cv_prep
+
+# evaluate model set on each fold
+cat('\n', blue$bold("Evaluating models"))
+tic()
+fold_res <- cv_prep |>
+  mutate(res = map2(train, test, ~ eval_model_set(models, .x, .y))) 
+toc()
+beepr::beep()
+
+fold_res
+rm(cv_prep)
+write_rds(fold_res, glue('./results/{out_path}/fold_res.rds'), compress = 'gz')
+
+
+# summarize metrics across folds for ML models
+cat('\n', blue$bold("Summarizing results"))
+
+cv_res <- fold_res |>
+  select(-test, -train) |> 
+  unnest(res) |> 
+  group_by(model_type, model_id, .metric) |>
+  summarize(
+    mean = mean(.estimate, na.rm = T),
+    err = sd(.estimate, na.rm = T),
+    n_folds = sum(!is.na(.estimate)),
+    values = list(.estimate),
+    .groups = 'drop'
+  )
+cv_res
+
+# evaluate simple rule-based classification
+thresh_res <- fold_res |> 
+  mutate(threshold_res = map2(train, test, ~eval_threshold_classififer(.x, .y))) |> 
+  select(outer_id, threshold_res) |> 
+  unnest(threshold_res) |> 
+  mutate(model_type = 'score & threshold rule') |> 
+  group_by(model_type, .metric) |>
+  summarize(
+    mean = mean(.estimate, na.rm = T),
+    err = sd(.estimate, na.rm = T),
+    n_folds = sum(!is.na(.estimate)),
+    values = list(.estimate),
+    .groups = 'drop'
+  )
+
+cv_res <- bind_rows(cv_res, thresh_res)
+
+write_rds(cv_res, glue('./results/{out_path}/cv_res.rds'), compress = 'gz')
+
+## select which model is best... check plots first too
+
+
+top_dogs <- cv_res |> 
+  group_by(model_type) |> 
+  filter(.metric == 'mcc') |> 
+  filter(mean - err == max(mean - err, na.rm = T)) 
+  
+write_rds(top_dogs, './results/3x3_regular_CV_07-02/best_models.rds')
+
+t_test_df <- 
+  top_dogs |> 
+  ungroup() |> 
+  select(model_type, model_id, values) |> 
+  transmute(model = paste0(model_type, model_id),
+            values)
+
+t_test_df |>
+  set_names(c('model_1', 'values_1')) |> 
+  crossing(t_test_df |> set_names(c('model_2', 'values_2')), 
+           .name_repair = 'universal') |>  
+  filter(model_1 != model_2) |> 
+  mutate(comparision = paste0(sort(c(model_1, model_2))))
---
+++ R/3_fit_final_model.R
@@ -0,0 +1,125 @@
+
+library(tidyverse)
+library(tidymodels)
+library(Biostrings)
+library(furrr)
+library(DECIPHER)
+library(glue)
+library(here)
+library(tictoc)
+library(crayon)
+
+source('./R/00_functions.R')
+source('./R/00_get_model_specs.R')
+
+set.seed(12345)
+
+folder <- 'final_model_07-04'
+system(glue('mkdir ./results/{folder}/'))
+system(glue('mkdir ./results/{folder}/align/'))
+system(glue('mkdir ./results/{folder}/hmm/'))
+system(glue('mkdir ./results/{folder}/hmmsearch/'))
+
+train <- read_rds('./data/classif_train_set.rds')
+test <- read_rds('./data/classif_test_set.rds')
+
+
+## Functions ----
+
+# Wrapper to apply align_domains to each subfamily
+build_alignments_library2 <- function(domains, save_path){
+  # do align_domains in parallel
+  plan(multisession, workers = availableCores())
+  domains |> mutate(
+    aligned = future_map(
+      .x = data, 
+      .f = ~ align_domains(.x, dest = save_path),
+      .options = furrr_options(scheduling = Inf)
+    )
+  )
+}
+
+# create paths, build call, do calls to hmmbuild for each subfamily
+build_hmm_library2 <- function(msa_path, hmm_path){
+  
+  temp <- tempfile()
+  plan(multisession, workers = availableCores())
+  
+  tibble(msa_path = Sys.glob(glue('{msa_path}/*'))) |> 
+    mutate(
+      hmm_path = str_replace_all(msa_path, 'align|aln', 'hmm'),
+      hmmbuild_call = glue('hmmbuild -o {temp} {hmm_path} {msa_path}'),
+      hmmbuild = future_map_dbl(hmmbuild_call, ~ system(.x))
+    )
+}
+
+# score a dataframe of sequences against the HMM library with hmmsearch
+# takes protein seq column and makes temporary fasta file to send to hmmer
+hmmsearch_scores2 <- function(df, hmm_path, tag){
+  
+  # make temp fasta file of seqs to score against hmm library
+  fasta_path <- tempfile()
+  fasta <- Biostrings::AAStringSet(df$prot_seq)
+  names(fasta) <- df$acc
+  writeXStringSet(fasta, fasta_path)  
+  
+  junk <- tempfile()
+  # setup paths for hmms and table outputs; build hmmsearch calls
+  hmmsearches <- 
+    tibble(hmm_path = Sys.glob(glue('{hmm_path}*'))) |> 
+    mutate(
+      out_path = hmm_path |> 
+        str_replace('/hmm/', '/hmmsearch/') |> 
+        str_replace('\\.hmm', glue('.{tag}.tbl')),
+      calls = glue('hmmsearch --noali -o {junk} --tblout {out_path} {hmm_path} {fasta_path}')
+    )
+  # do hmmsearch calls in parallel
+  plan(multisession, workers = availableCores())
+  hmmsearches <- hmmsearches |> 
+    mutate(hmmsearches = future_map_dbl(calls, ~ system(.x))) 
+  return(hmmsearches)
+}
+
+
+## Main: Prep ----
+tic()
+# do alignments
+cat(white(' Doing alignments ... \n\n'))
+aligns <- 
+  prep_domains_df(train) |> 
+  build_alignments_library2(save_path = glue('./results/{folder}/align/'))
+toc()
+
+
+# train hmms
+cat(white(' Building HMMs ...\n\n'))
+hmm_check <- build_hmm_library2(msa_path =  glue('./results/{folder}/align/'),
+                                hmm_path = glue('./results/{folder}/hmm/'))
+
+hmmbuild_test <- all(hmm_check$hmmbuild == 0)
+
+cat('\n', white(glue('hmmbuild calls all finished without error?\n {hmmbuild_test}')))
+
+
+# hmm scores test and train
+cat('\n', white(' Scoring sequences against HMM library...'))
+tic()
+train_prep <-
+  hmmsearch_scores2(train, hmm_path = glue('./results/{folder}/hmm/'), tag = 'train')
+test_prep <-
+  hmmsearch_scores(test, hmm_path = glue('./results/{folder}/hmm/'), tag = 'test')
+toc()
+
+
+## prepared data
+write_rds(train_prep, './train_prep.rds')
+write_rds(test_prep, './test_prep.rds')
+
+
+
+
+## Fit models
+
+
+
+## Evaluate
