5f101d331377b68ab1434b88ce127bf18ab08316
jmoggridge <ja.moggridge@gmail.com>
2021-07-06 00:53:56 -0400

    starting to work on parsing genpept files

--- R/Parse_CDD_genpept.R
+++ R/Parse_CDD_genpept.R
@@ -1,6 +1,13 @@
 ## Part 2
 
-
+library(tidyverse)
 # Conserved domains specific proteins...
-gp <- './data/CDD/RitA_specific.gp'
-read_lines(file)
+gp <- './data/CDD/sample.gp'
+gp <- read_file(gp)
+
+gp_split <- str_split(gp, '\\n//\\n')
+
+str_count(gp_split, '\\')
+
+length(gp_split)
+gp_split
