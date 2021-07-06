d83232a3092e92a9c1bcae4b1875fa7f3288169d
jmoggridge <ja.moggridge@gmail.com>
2021-07-06 02:33:19 -0400

    memory overload problem seems to be fixed now

--- R/3_final_test_set.R
+++ R/3_final_test_set.R
@@ -11,10 +11,11 @@ library(crayon)
 
 source('./R/00_functions.R')
 source('./R/00_get_model_specs.R')
+options(tibble.print_max = 50)
 
 set.seed(12345)
 
-folder <- 'final_model_07-04'
+folder <- 'final_model_07-05'
 system(glue('mkdir ./results/{folder}/'))
 system(glue('mkdir ./results/{folder}/align/'))
 system(glue('mkdir ./results/{folder}/hmm/'))
@@ -81,21 +82,44 @@ hmmsearch_scores2 <- function(df, hmm_path, tag){
 }
 
 
+# create a workflow for model spec with recipe, then fit to training set
+fit_workflow <- function(spec, wkfl, train_prep) {
+  wkfl |>  
+    add_model(spec) |>  
+    fit(data = train_prep)
+}
+
+# predict test data
+get_predictions <- function(fitted_wkfl, test){
+  fitted_wkfl |>
+    predict(test |> select(-subfamily))
+}
+# specify evaluation functions
+collect_pref_metrics <- function(preds, truth, met_set){
+  preds <- preds$.pred_class
+  my_metrics <- metric_set(mcc, kap, sensitivity, specificity, precision, recall, 
+                           f_meas, ppv, npv, accuracy, bal_accuracy)
+  tibble(t=truth, p=preds) |> 
+    my_metrics(estimate = p, truth = t)
+}
+
+
 ## Main: Prep ----
-tic()
+
+
 # do alignments
+tic()
 cat(white(' Doing alignments ... \n\n'))
 aligns <- 
   prep_domains_df(train) |> 
   build_alignments_library2(save_path = glue('./results/{folder}/align/'))
 toc()
 
-
 # train hmms
 cat(white(' Building HMMs ...\n\n'))
 hmm_check <- build_hmm_library2(msa_path =  glue('./results/{folder}/align/'),
                                 hmm_path = glue('./results/{folder}/hmm/'))
-
+hmm_check
 hmmbuild_test <- all(hmm_check$hmmbuild == 0)
 cat('\n', white(glue('hmmbuild calls all finished without error?\n {hmmbuild_test}')))
 
@@ -109,124 +133,93 @@ test_scores <-
   hmmsearch_scores2(test, hmm_path = glue('./results/{folder}/hmm/'), tag = 'test')
 toc()
 
+
 # parse hmmscores and add to data frame
 train_prep <- train |> join_hmmsearches(files = train_scores$out_path)
 test_prep <- test |> join_hmmsearches(files = test_scores$out_path)
 
 ## prepared data
-prep <- tibble(train_prep = list(train_prep), 
-                test_prep = list(test_prep))
+prepped_data <- tibble(train_prep = list(train_prep), 
+                       test_prep = list(test_prep))
+prepped_data
 
-write_rds(prep, './results/final_prep.rds')
-write_rds(aligns, './results/final_train_aligns.rds')
+write_rds(prepped_data, glue('./results/{folder}/final_prep.rds'))
+# write_rds(aligns, './results/final_train_aligns.rds')
 
 rm(train, test, train_scores, test_scores, hmm_check)
 
 
-prep <- read_rds('./results/final_prep.rds')
+## -----
 
-train_prep <- prep$train[[1]]
-test_prep <- prep$test[[1]]
+prepped_data <- read_rds(glue('./results/{folder}/final_prep.rds'))
 
+train_prep <- prepped_data$train_prep[[1]]
+test_prep <- prepped_data$test_prep[[1]]
+rm(prepped_data)
+
+train_prep |> count(subfamily)
+test_prep |> count(subfamily)
 
 # specify modelling workflow with scaling and ignore seqs and id
-recip <- recipe(subfamily ~ ., data = train_prep) |> 
+recip <- 
+  recipe(subfamily ~ ., data = train_prep) |> 
   update_role(c('acc', contains('seq')), new_role = "id") |> 
   step_scale(all_predictors())
 
 # see scaled data
-recip |> prep() |> juice()
+recip |> prep() |> juice() |> skimr::skim()
 
 wkfl <- workflow() |> 
   add_recipe(recip)
 wkfl
 
+# selected models from 2c_regular_CV.R
+best_models <- 
+  read_rds('./results/3x3_regular_CV_07-02/best_models.rds') |> 
+  filter(!is.na(model_id))  |>
+  nest(reg_cv_res = c(.metric, mean, err, n_folds, values)) |> 
+  left_join(models, by = c("model_type", "model_id"))
 
-# create a workflow for model spec with recipe, then fit to training set
-fit_workflow <- function(spec, wkfl, train_prep) {
- return(wkfl |>  add_model(spec) |>  fit(data = train_prep))
-}
 
-# apply fitting to all models
+## Fit, predict, collect metrics ------
+
+cat('\n', white('Fitting, predicting, and evaluating performance'))
 plan(multisession, workers = 8)
+fitted_models <- 
+  models |> 
+  mutate(
+    # fit each model to the prepared training data
+    fitted_wkfl = future_map(
+      .x = spec, 
+      .f = ~fit_workflow(.x, wkfl, train_prep),
+      .options = furrr_options(
+        packages = c("parsnip","tidymodels", "glmnet","rpart", "kknn"),
+        seed = NULL)
+    ),
+    # get predictions from each model for final test set
+    preds = future_map(fitted_wkfl, ~get_predictions(.x, test_prep)),
+    # evaluate prediction performance
+    metrics = future_map(preds, ~collect_pref_metrics(preds = .x, truth = test_prep$subfamily))
+    ) |> 
+  select(-spec)
 
-fitted_models <- models |> 
-  mutate(fitted_wkfl = future_map(
-    .x = spec, 
-    .f = ~fit_workflow(.x, wkfl, train_prep),
-    .options = furrr_options(
-      packages = c("parsnip","tidymodels", "glmnet","rpart", "kknn"),
-      seed = NULL)
-    ))
 beepr::beep()
 
 fitted_models
-fitted_models$fitted_wkfl[[1]]
-fitted_models$fitted_wkfl[[21]]
-fitted_models$fitted_wkfl[[41]]
-fitted_models$fitted_wkfl[[43]]
-
-
-# predict test data
-get_predictions <- function(fitted_wkfl, test){
-  predictions <- 
-    fitted_wkfl |>
-    predict(test |> select(-subfamily)) %>%
-    bind_cols(test |> select(subfamily))
-}
-
-# apply get_predictions to each fitted workflow
-plan(multisession, workers = 8)
-
-fitted_models_w_preds <-  fitted_models |> 
-  mutate(preds = future_map(
-    .x = fitted_wkfl, 
-    .f = ~get_predictions(.x, test_prep),
-    .options = furrr_options(
-      packages = c("parsnip","tidymodels", "glmnet","rpart", "kknn"),
-      seed = NULL)
-    ))
-
-beepr::beep()
-
-# specify evaluation functions
-class_metrics <- 
-  metric_set(mcc, kap, sensitivity, specificity, precision, recall, 
-             f_meas, ppv, npv, accuracy, bal_accuracy)
-
-
-
-
-## Evaluate prediction performance
-
 
 
 ## Fit/evaluate rule-based classifier
-thresh_res <- prep |>
-  mutate(threshold_res = map2(train, test, ~eval_threshold_classififer(.x, .y))) |>
+
+prepped_data <- read_rds(glue('./results/{folder}/final_prep.rds'))
+
+thresh_res <- prepped_data |>
+  mutate(threshold_res = map2(train_prep, test_prep, ~eval_threshold_classififer(.x, .y))) |>
   select(threshold_res) |>
   unnest(threshold_res) |>
   mutate(model_type = 'score & threshold rule',
          model_id = NA)
 
-# final_test_res <- 
-#   bind_rows(thresh_res, metrics) |> 
-#   mutate(model_id = model_id)
-# rm(thresh_res)
-# 
-# final_test_res |> View()
-# 
-# 
-# 
-# 
+
 # 
 # best_in_cv <- 
 #   read_rds('./results/3x3_regular_CV_07-02/best_models.rds')
-# 
-# best_in_cv |> 
-#   left_join(final_test_res)
-#   
-# 
-# 
-# final_test_res |> 
-#   ggplot(aes(x = estimate, y = fct_rev(model_type)))
