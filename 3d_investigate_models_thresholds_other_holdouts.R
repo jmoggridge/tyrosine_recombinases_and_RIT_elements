b22add1b8b1362c8a3bb59ce92a92218dac51623
jmoggridge <ja.moggridge@gmail.com>
2021-07-12 23:17:09 -0400

    various additions

---
+++ 3d_investigate_models_thresholds_other_holdouts.R
@@ -0,0 +1,10 @@
+
+## Investigate the final train / validation split....
+# do a confusion matrix for each classifier....
+
+
+## Investigate thresholds
+# unnormalized
+# normalized
+# normalized with smote to add examples to minority classes
+# which plot normalized thresholds...
