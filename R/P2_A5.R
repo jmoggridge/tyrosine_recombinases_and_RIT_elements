17dfa78fbf8ef9079813dd20d208a87f322c419a
jmoggridge <ja.moggridge@gmail.com>
2021-07-16 22:24:26 -0400

    open_genbank() done; next classify_protein(); then identify_rits()

--- R/P2_A5.R
+++ R/P2_A5.R
@@ -7,13 +7,104 @@ library(rentrez)
 source('./R/P2_parse_genbank_xml.R')
 set_entrez_key('889fdb9786a14019a0a1257196a09ba4ba08')
 Sys.getenv('ENTREZ_KEY')
+# genbank files for these nuc ids lack the CDS features.
+no_cds <- c('1834417989', '1024364864')
 
-## These genbank records have no CDS included.... problem!
-# no_CDS <- c('1834417989', '1024364864')
-
+# these nucleotides probably contain RIT element bc have 3 CDD proteins.
 three_ints <- read_rds('./data/CDD/ids_w_three_integrases.rds') |> 
+  filter(!nuc_id %in% no_cds) |> 
   arrange(slen)
 three_ints
+
+# prot_id and accessions for this set of nt's.
+three_ints_prots <- three_ints |> unnest(prot_id) |> pull(prot_id)
+
+# join with other accession numbers
+prot_accessions <- read_rds('./data/CDD/prot_summary_fixed.rds') |> 
+  filter(prot_id %in% three_ints_prots) |> 
+  select(prot_id, caption)
+
+# create an index for nuc ids to their genbank records
+genbank_files_index <- 
+  tibble(file = Sys.glob('./data/CDD/RIT_gbk_[0-9].rds')) |> 
+  mutate(nuc_id = map(file, ~ read_rds(.x) |> pull(nuc_id))) |> 
+  unnest(nuc_id)
+
+
+
+# for a nuc_id, return the parsed genbank record as a tibble
+open_genbank <- function(x){
+  # figure out which file to open from index
+  file <- genbank_files_index |> 
+    filter(nuc_id == x) |> 
+    pull(file)
+  file
+  
+  # pull the genbank record and parse
+  gbk <- read_rds(file) |> 
+    filter(nuc_id == x) |> 
+    unnest(gbk) |> 
+    pull(gbk) |> 
+    parse_genbank()
+  return(gbk)
+}
+
+# example: first nuc_id
+x <- three_ints$nuc_id[[1]]
+gbk <- open_genbank(x)
+
+# are the three RIT proteins there??
+prot_x <- three_ints |>
+  filter(nuc_id == x) |> 
+  unnest(prot_id) |> pull(prot_id)
+cap_x <- prot_summary |> 
+  filter(uid %in% prot_x) |> 
+  pull(caption) |>
+  map_chr(~glue('{.x}.1'))
+
+# extract features table
+ft <- gbk |>
+  select(feature_table) |> 
+  unnest(feature_table) |> 
+  select(-gb_feature_key, -locus_tag)
+ft |> 
+  View()
+
+
+which(ft$protein_id %in% cap_x)
+
+
+# classify proteins
+
+classify_proteins <- function(gbk){
+  
+  
+}
+
+## HMM scores
+# make temp fasta file of seqs to score against hmm library
+fasta_path <- tempfile()
+fasta <- Biostrings::AAStringSet(gbk$translation)
+names(fasta) <- gbk${{protein}}
+writeXStringSet(fasta, fasta_path)  
+
+
+
+# figure out where there is three RITs in a row...
+
+
+gbk_parsed <- parse_genbank(gbk)
+gbk_parsed
+feature_table <- gbk_parsed |>
+  select(feature_table) |> 
+  unnest(feature_table)
+gbk_prot_id <- feature_table |> pull(protein_id)
+
+prot_for_nuc_id <- 
+  three_ints |> 
+  filter(nuc_id == x) |> 
+  pull(prot_id)
+any(gbk_prot_id)
 
 
 # for one nucleotide -> get genbank -> identify RITs
