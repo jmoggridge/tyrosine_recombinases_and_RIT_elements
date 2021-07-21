ab5aeb50084ca3e6eff662b2eb0c7d0c932cd4dc
jmoggridge <ja.moggridge@gmail.com>
2021-07-21 19:27:10 -0400

    2 issues with new download set <- c('595887121', '1305031314')

--- R/P2_C1_two_integrases_genbank_downloads.R
+++ R/P2_C1_two_integrases_genbank_downloads.R
@@ -1,15 +1,38 @@
 ## P2_XX: Extract probable RIT elements using ID database
 
 library(tidyverse)
+library(rentrez)
 library(progress)
 library(glue)
-library(rentrez)
+library(beepr)
+
 source('./R/P2_parse_genbank_xml.R')
 set_entrez_key('889fdb9786a14019a0a1257196a09ba4ba08')
 Sys.getenv('ENTREZ_KEY')
 dir.create('./data/CDD/genbank_2integrases/')
 
-#
+## DL 1: missing CDS ------------------------------------------------------
+
+## previously missing feature table
+# missing_cds <- read_rds('./data/CDD/rit_finder/missing_cds.rds') |> 
+#   select(nuc_id)
+# 
+# ## fetch genbank records to see if CDS now present
+# pb <- make_pb(nrow(missing_cds))
+# missing_cds_fixed <- missing_cds |> 
+#   mutate(gbk = map(nuc_id, ~{
+#     print(.x)
+#     gb <- fetch_genbank_with_parts(.x)
+#     pb$tick()
+#     return(gb)
+#   }))
+# beep()
+# missing_cds_fixed
+# write_rds(missing_cds_fixed, 'data/CDD/RIT_gbk_noCDS_fixed.rds')
+# rm(missing_cds_fixed)
+
+## DL 2: two integrases ------------------------------------------------------
+
 id_data <- read_rds('./data/CDD/id_data_fixed.rds')
 nuc_summary <- read_rds('./data/CDD/nuc_summary_fixed.rds')
 cdd_titles <- read_rds('./data/CDD/cdd_summary.rds') |> 
@@ -44,16 +67,14 @@ two_integrases <-
   left_join(
     nuc_summary |> select(nuc_id, slen, organism, caption)
   )
+write_rds(two_integrases, './data/CDD/ids_with_2_integrases.rds')
 
 # smallest nucleotides first; check which CD are present
 two_integrases |>
   arrange(slen) |>  
   unnest(c(prot_id, cdd_title)) # |> View()
-
 two_integrases |> unnest(c(prot_id, cdd_title)) |>  print(n=50)
 
-
-write_rds(two_integrases, './data/CDD/ids_with_2_integrases.rds')
 
 # how many nucleotides for a given taxa have > 3 RIT integrases?
 nucs_by_taxid <- 
@@ -77,37 +98,19 @@ tot <- two_integrases |>
   nrow()
 tot
 
+
+
+## Get 2058 records with 2 CDD hits =============================
+
 # skip these for now... problems parsing these genbank records
-issues <- c()
-
-
-## DOWNLOADS ------------------------------------------------------------------
-
-## previously missing feature table
-missing_cds <- read_rds('./data/CDD/rit_finder/missing_cds.rds') |> 
-  select(nuc_id)
-
-# fetch genbank records to see if CDS now present
-pb <- make_pb(nrow(missing_cds))
-missing_cds_fixed <- missing_cds |> 
-  mutate(gbk = map(nuc_id, ~{
-    print(.x)
-    gb <- fetch_genbank_with_parts(.x)
-    pb$tick()
-    return(gb)
-  }))
-beep()
-missing_cds_fixed
-write_rds('data/CDD/RIT_gbk_noCDS_fixed.rds')
-
-gbks <- read_rds('data/CDD/RIT_gbk_noCDS_fixed.rds')
-
-# 2058 records with 2 CDD RIT (A,B,C) =============================
+issues <- c('595887121', '1305031314')
 
 # 1,491 taxa not represented in rit elements already
 to_retrieve1 <- 
   two_integrases |> 
-  filter(!tax_id %in% c(read_rds('./results/rit_elements.rds') |> pull(tax_id))) |> 
+  filter(!tax_id %in% c(
+    read_rds('./results/rit_elements.rds') |> pull(tax_id)
+    )) |> 
   select(nuc_id, tax_id) |> 
   filter(!nuc_id %in% issues) |> 
   distinct()
@@ -117,37 +120,40 @@ to_retrieve2 <-
   two_integrases |> 
   filter(!nuc_id %in% to_retrieve1$nuc_id)
 
-rm(cdd_titles, count_table, id_data, nucs_by_taxid, 
-   nuc_summary, tot, two_integrases)
+rm(cdd_titles, count_table, id_data, nucs_by_taxid, nuc_summary, tot, two_integrases)
 
 
 # processes a batch of requests and then saves the raw genbank data in the same row as id. No return value, just makes a file
 fetch_gb_set <- function(df, id_var, start, stop, folder){
   pb <- make_pb(n = stop - start + 1)
   records <- df |> 
+    filter(!nuc_id %in% issues) |> 
     slice(start:stop) |> 
-    mutate(gbk = map({{nuc_id}}, ~{
+    mutate(gbk = map(nuc_id, ~{
       cat('\n')
-      print(glue('id = .x'))
+      print(glue('id = {.x}'))
       gb <- fetch_genbank_with_parts(.x)
       pb$tick()
       return(gb)
     }))
   print(glimpse(records))
+  print('Saving to file:')
+  print(glue('{folder}/GB_w_parts_{start}_{stop}.rds'))
   write_rds(records, glue('{folder}/GB_w_parts_{start}_{stop}.rds'))
   beepr::beep()
 }
 
 # do fetching for sets of 100 ids.
 purrr::walk(
-  .x = seq(1, nrow(two_integrases), 100),
+  .x = seq(1, nrow(to_retrieve1), 100),
   .f = ~fetch_gb_set(
-    df = two_integrases, 
-    id_var = nuc_id, 
+    df = to_retrieve1, 
     start = .x, stop = .x + 99,
     folder = 'data/CDD/genbank_2integrases'
     )
 )
 
+## files look like:
+# data/CDD/genbank_2integrases/GB_w_parts_401_500.rds
 
 
